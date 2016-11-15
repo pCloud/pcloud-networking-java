408913d39f54c1d1ba33ed2d33536eb3101ef415
Georgi Neykov <georgi.neykov@gmail.com>
2016-11-15 23:41:52 +0200

    Update copyright notice

--- src/main/java/com/pcloud/BinaryProtocolCodec.java
+++ src/main/java/com/pcloud/BinaryProtocolCodec.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2016 Georgi Neykov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.pcloud;
 
 import com.pcloud.value.*;
@@ -13,10 +29,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-
-/**
- * Created by Georgi Neykov on 11/10/2016.
- */
 
 class BinaryProtocolCodec {
 
