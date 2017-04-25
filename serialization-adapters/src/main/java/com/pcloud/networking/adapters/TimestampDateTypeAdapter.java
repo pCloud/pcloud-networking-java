047c2ced18c8ff478a38c4b5cf5aee0229819ace
Georgi Neykov <georgi.neykov@pcloud.com>
2017-04-25 10:58:18 +0300

    (Serialization) Add a module with additional TypeAdapter implementations

---
+++ serialization-adapters/src/main/java/com/pcloud/networking/adapters/TimestampDateTypeAdapter.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2017 pCloud AG
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.pcloud.networking.adapters;
+
+import com.pcloud.networking.TypeAdapter;
+import com.pcloud.protocol.streaming.ProtocolReader;
+import com.pcloud.protocol.streaming.ProtocolWriter;
+
+import java.io.IOException;
+import java.util.Date;
+
+public class TimestampDateTypeAdapter extends TypeAdapter<Date> {
+    @Override
+    public Date deserialize(ProtocolReader reader) throws IOException {
+        return new Date(reader.readNumber() * 1000L);
+    }
+
+    @Override
+    public void serialize(ProtocolWriter writer, Date value) throws IOException {
+        writer.writeValue(value.getTime() / 1000L);
+    }
+}
